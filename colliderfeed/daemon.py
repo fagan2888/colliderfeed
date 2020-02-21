dfa5c4569f63ba4a4525812c6bcf57c2b9801b19
microprediction <info@3za.org>
2020-02-21 13:13:02 -0500

    Initial commit. Example data feed, untested.

---
+++ colliderfeed/daemon.py
@@ -0,0 +1,79 @@
+from colliderfeed.collider_feed_config_private import COLLIDER_FEED_CONFIG as config
+import asyncio, aiohttp, json, requests, schedule, time
+from retrying import retry
+from contexttimer import Timer
+
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        if response.status != 200:
+            response.raise_for_status()
+        return await response.text()
+
+async def fetch_all(session, urls):
+    tasks = []
+    for url in urls:
+        task = asyncio.create_task(fetch(session, url))
+        tasks.append(task)
+    results = await asyncio.gather(*tasks)
+    return results
+
+async def fetch_prices(symbols):
+    urls = [ config["template_url"].replace("SYMBOL",symbol) for symbol in symbols ]
+    async with aiohttp.ClientSession() as session:
+        results = await fetch_all(session, urls)
+    prices = [json.loads(r).get('Global Quote')['05. price'] for r in results]
+    return prices
+
+def retry_if_result_none(result):
+    """Return True if we should retry (in this case when result is None), False otherwise"""
+    return result is None
+
+@retry(wait_exponential_multiplier=1000, wait_exponential_max=10000,retry_on_result=retry_if_result_none)
+def collider_prices(symbols):
+    """ Get prices from market data feed """
+    prices =  asyncio.run( fetch_prices(symbols=symbols ) )
+    if prices is None:
+        raise Exception("Feed may be down")
+    return prices
+
+def set_or_touch(names,write_keys,budgets, values=None, touch=True):
+    """ Use web API to set or touch names """
+    base_url = "microprediction.pythonanywhere.com/multi/"
+    request_data = {"names": ",".join(names), "write_keys": ",".join(write_keys),
+                    "budgets": ",".join([str(b) for b in budgets])}
+    if touch:
+        res = requests.put(base_url,data=request_data)
+    else:
+        if values:
+            request_data.update( {"values":",".join( values)}  )
+            res = requests.put(base_url, data=request_data)
+        else:
+            return None
+    return res.status_code==200
+
+prev_prices = None
+def minutely_feed_task(symbols, names, write_keys, budgets ):
+    """ Modify feed of price changes or at least keep it warm """
+    global prev_prices
+    with Timer() as t_get:
+        prices = collider_prices(symbols=symbols)
+    print( str(t_get.elapsed)+' ms getting data.')
+
+    if prev_prices is None or prices is None:
+        set_or_touch(touch=True, names=names, write_keys=write_keys, budgets=budgets)
+    else:
+        price_changes = [ p-pp for p,pp in zip(prices,prev_prices) ]
+        with Timer() as t_put:
+            set_or_touch(touch=False, names=names, write_keys=write_keys, values=price_changes, budgets=budgets)
+        print(str(t_put.elapsed)+ ' ms putting data.')
+
+        prev_prices = [ p for p in prices ]
+
+
+if __name__=="__main__":
+    schedule.every(1).minutes.do(minutely_feed_task)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
